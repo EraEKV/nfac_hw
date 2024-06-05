ca8e8de501dec9aa13e912501c3727c8fd6e94a9
Erasyl Kokenov <kokenaoerasyl@gmail.com>
2024-06-06 02:27:52 +0500

    second

--- src/app/components/cards.jsx
+++ src/app/components/cards.jsx
@@ -7,7 +7,7 @@ const Cards = () => {
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
         {data.map((item) => (
             <Card
-                id={item.id}
+                key={item.id}
                 title={item.title}
                 date={item.date}
                 name={item.author}
